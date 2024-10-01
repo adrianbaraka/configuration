27a6abfa2ade618c960ad32d8175f1013840ec6e
Adrian <adrianbaraka004@gmail.com>
2024-10-01 11:04:17 +0300

    add times new roman font

--- Setup/adm-setup.sh
+++ Setup/adm-setup.sh
@@ -33,6 +33,23 @@ sudo apt-get update && sudo apt-get install spotify-client
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.
 
+#Firefox
+sudo install -d -m 0755 /etc/apt/keyrings
+
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+
+gpg -n -q --import --import-options import-show /etc/apt/keyrings/packages.mozilla.org.asc | awk '/pub/{getline; gsub(/^ +| +$/,""); if($0 == "35BAA0B33E9EB396F59CA838C0BA5CE6DC6315A3") print "\nThe key fingerprint matches ("$0").\n"; else print "\nVerification failed: the fingerprint ("$0") does not match the expected one.\n"}'
+
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+
+echo '
+Package: *
+Pin: origin packages.mozilla.org
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla 
+
+sudo apt-get update && sudo apt-get install firefox
+
 #Only Office
 mkdir -p ~/.gnupg
 chmod 700 ~/.gnupg
@@ -84,7 +101,10 @@ xfconf-query --create -c 'xfce4-keyboard-shortcuts' -p '/commands/custom/WLAN' -
 sudo cp -v conf-files/wallpapers/spiderman-upside.png /boot/grub
 
 sudo bash -c 'echo "GRUB_BACKGROUND=/boot/grub/spiderman-upside.png" >> /etc/default/grub'
-update-grub
+sudo update-grub
 
+#########################################################################################################################################################################
+#Times new roman font
+sudo mkdir -p "/usr/share/fonts/Times New Roman"
 
-
+sudo cp -vr "conf-files/Times New Roman"/* "/usr/share/fonts/Times New Roman"
